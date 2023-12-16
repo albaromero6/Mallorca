674ad0eb8b99fa9070cfd39afeff93192bc9cd02
Alba Romero <137055664+albaromero6@users.noreply.github.com>
2023-12-16 13:02:36 +0100

    Add files via upload

---
+++ js/funcion.js
@@ -0,0 +1,69 @@
+/****************************************************************/
+/*              Java Script for Islas Baleares web              */
+/*                     Author: Alba Romero                      */
+/*                         Versión: 1.0                         */
+/*                     Date: December 2023                      */
+/*                   Comments: Class project                    */
+/****************************************************************/
+
+/***************************NAV FUCTION**************************/
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+/***************************SLIDE FUCTION************************/
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+/************************MODAL FUCTION****************************/
+
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
